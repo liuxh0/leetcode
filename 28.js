f915b4eae287fd0c8d37e7016387dbd57514a813
Xinhu Liu <xinhu.liu90@gmail.com>
2019-07-29 23:32:30 +0200

    Add #28

---
+++ 28.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+    if (haystack.substr(i, needle.length) === needle) {
+      return i;
+    }
+  }
+
+  return -1;
+};
