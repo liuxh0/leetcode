e9a31ccc6ae0a4de9d4ac297d008f6b933121f73
Xinhu Liu <xinhu.liu90@gmail.com>
2020-03-05 19:39:11 +0100

    Add #108

---
+++ 108.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+  if (nums.length === 0) {
+      return null;
+  }
+
+  const mid = Math.ceil((nums.length - 1) / 2);
+  const node = new TreeNode(nums[mid]);
+  node.left = sortedArrayToBST(nums.slice(0, mid));
+  node.right = sortedArrayToBST(nums.slice(mid + 1));
+  return node;
+};
